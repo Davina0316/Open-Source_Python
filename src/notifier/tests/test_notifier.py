bc7867b0fefb558491456b75de34523b89067d69
Davina0316 <zg2501@nyu.edu>
2025-03-05 17:07:36 -0500

    Initial commit with all changes

---
+++ src/notifier/tests/test_notifier.py
@@ -0,0 +1,7 @@
+from src.notifier.notifier import Notifier
+
+
+def test_notifier():
+    notifier = Notifier(threshold=10)
+    notifier.notify(15)
+    assert notifier.get_notifications() == ["Alert: Result 15 exceeds threshold 10"]
